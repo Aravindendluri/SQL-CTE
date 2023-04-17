4f4bd39970e5597a09e6e7842bf871fa8b88d8af
Aravindendluri <103320490+Aravindendluri@users.noreply.github.com>
2023-04-17 13:36:56 +0200

    Delete CTE .sql

--- CTE .sql
+++
@@ -1,8 +0,0 @@
-
-	with cte as (select title, release_year, (revenue-budget) as profit
-			from movies m
-			join financials f
-			on m.movie_id=f.movie_id
-			where release_year>2000 and industry="hollywood"
-	)
-	select * from cte where profit>500
